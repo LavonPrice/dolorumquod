01c887ad649f1555979a1d33e59b4b09f6706544
LavonPrice <dolorescasey69fb4a72598@outlook.com>
2024-03-23 01:41:45 +0800

    unde

---
+++ routes/user_signature.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserSignature = void 0;
+/**
+ * This class represents the Zoho CRM User.
+ */
+class UserSignature {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+exports.UserSignature = UserSignature;
+//# sourceMappingURL=user_signature.js.map
